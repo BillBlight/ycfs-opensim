e6d8d8a14d27a535ae67889e6f9ba0089e828340
Bill Blight <watchersphone@gmail.com>
2020-08-18 16:16:20 +0000

    Add my buildit.sh bash shell script

---
+++ buildit.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+nant /clean
+echo "Scrubbing Directories"
+find . -name "*.csproj" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.csproj.user" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.build" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*Temporary*" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.cache" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.rej" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.orig" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.pdb" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.mdb" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.bak" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.obj" -type f -print0 | xargs -0 /bin/rm -rf
+find . -name "*obj" -type d -print0 | xargs -0 /bin/rm -rf
+
+echo "Running Prebuild"
+./runprebuild.sh
+
+echo "Building Release, MS Debug Info sucks on linux"
+msbuild /p:Configuration=Release /verbosity:minimal
+BUILDCODE=$?
+
+
+if [ $BUILDCODE -gt 0 ]; then
+echo -e "\e[41m
+
+The Build encoutered and error.\e[0m
+
+"
+exit 1
+else
+echo -e "
+\e[30;48;5;82m
+The Build appears to have suceeded\e[0m
+"
+fi
+echo "done"
