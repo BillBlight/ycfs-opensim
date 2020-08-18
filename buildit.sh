c39399873c520fb4c53d15eb97a47f8a66c0b3c6
Bill Blight <watchersphone@gmail.com>
2020-08-18 16:50:51 +0000

    Update buildit.sh make it pretty and switch to bash instead of sh

--- buildit.sh
+++ buildit.sh
@@ -1,5 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+echo -e "\e[41m Getting Avaialble CPU Cores \e[0m"
+NUMCORES=$(grep -c ^processor /proc/cpuinfo)
+echo -e "\e[41m CPU CORES: $NUMCORES \e[0m"
+NUMCORES=$(expr $NUMCORES / 4)
+if [ "$NUMCORES" -lt 1 ] ; then
+NUMCORES=1
 
+echo -e "\e[41m 
+Selecting $NUMCORES Core for msbuild. \e[0m"
+else
+echo -e "\e[41m 
+Selecting 25% of Cores for msbuild: $NUMCORES \e[0m"
+fi
+sleep 3
 echo "Scrubbing Directories"
 find . -name "*.csproj" -type f -print0 | xargs -0 /bin/rm -f
 find . -name "*.csproj.user" -type f -print0 | xargs -0 /bin/rm -f
@@ -18,7 +31,7 @@ echo "Running Prebuild"
 ./runprebuild.sh
 
 echo "Building Release, MS Debug Info sucks on linux"
-msbuild /p:Configuration=Release /verbosity:minimal
+msbuild -m:$NUMCORES /p:Configuration=Release /verbosity:minimal
 BUILDCODE=$?
 
 
